7e4f29f3978dc014821e5d73f4821c1fc60931a7
juanpormon <jd.portilla@uniandes.edu.co>
2022-05-07 23:04:17 -0500

    primeros cambios de juan, se agregan los features de kraken

--- kraken_ghost/features/web/step_definitions/step_Andres.js
+++ kraken_ghost/features/web/step_definitions/step_Andres.js
@@ -2,62 +2,62 @@ const { Given, When, Then } = require('@cucumber/cucumber');
 var {browser} = require('cucumber')
 
 
-Then('I enter  entre new post content {kraken-string}', async function (content) {
+When('I enter  entre new post content {kraken-string}', async function (content) {
     let element = await this.driver.$('[data-placeholder="Begin writing your post..."]')
     return await element.setValue(content);
 });
 
-Then('I click on Publish option', async function(){
+When('I click on Publish option', async function(){
     let element = await this.driver.$('#ember641')
     return await element.click();
 });
 
-Then('I click on Drafts', async function(){
+When('I click on Drafts', async function(){
     let element = await this.driver.$('#ember920')
     return await element.click();
 });
 
-Then('I enter to first draft', async function(){
+When('I enter to first draft', async function(){
     let element = await this.driver.$('#ember1157')
     return await element.click();
 });
 
-Then('I click on Published', async function(){
+When('I click on Published', async function(){
     let element = await this.driver.$('#ember1241')
     return await element.click();
 });
 
-Then('I enter to first published', async function(){
+When('I enter to first published', async function(){
     let element = await this.driver.$('#ember1318')
     return await element.click();
 });
 
-Then('I click on Update', async function(){
+When('I click on Update', async function(){
     let element = await this.driver.$('#ember1532')
     return await element.click();
 });
 
-Then('I click on Update option', async function(){
+When('I click on Update option', async function(){
     let element = await this.driver.$('#ember1587')
     return await element.click();
 });
 
-Then('I click on Pages', async function(){
+When('I click on Pages', async function(){
     let element = await this.driver.$('#ember1815')
     return await element.click();
 });
 
-Then('I click on New Page', async function(){
+When('I click on New Page', async function(){
     let element = await this.driver.$('#ember1826')
     return await element.click();
 });
 
-Then('I click on Scheduled', async function(){
+When('I click on Scheduled', async function(){
     let element = await this.driver.$('#ember2300')
     return await element.click();
 });
 
-Then('I click on Scheduled New post', async function(){
+When('I click on Scheduled New post', async function(){
     let element = await this.driver.$('#ember2341')
     return await element.click();
 });
