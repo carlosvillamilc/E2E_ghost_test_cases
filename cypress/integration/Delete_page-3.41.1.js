0311888b172aad5394cf167dc423b6618fd30765
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-05-24 23:11:33 -0500

    Correccion escenarios delete page v3 y v4

--- cypress/integration/Delete_page-3.41.1.js
+++ cypress/integration/Delete_page-3.41.1.js
@@ -2,6 +2,9 @@ let user = '';
 let password = '';
 let inviteUser='';
 let postTitle= '';
+let port = '3001';
+let index = 1;
+
 describe('Delete page', () => {
     beforeEach(()=>{        
         cy.fixture('ghost.json').as('ghost')
@@ -11,25 +14,49 @@ describe('Delete page', () => {
             inviteUser = data.inviteUser
             postTitle = data.postTitle
         })
-        cy.visit('http://localhost:3001/ghost/')
+        cy.visit(`http://localhost:${port}/ghost/`)
         cy.wait(2000)        
         
     })
     it('Login, Test Delete Page', () => {       
-        cy.get('#ember8').type(user)
+        cy.get('#ember8').type(user)        
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++
+        
         cy.get('#ember10').type(password)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++
         cy.get('#ember12').click()        
-        cy.url().should('eq', 'http://localhost:2368/ghost/#/site')
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++
+        cy.url().should('eq', `http://localhost:${port}/ghost/#/site`)
         cy.get('a[href="#/pages/"]').eq(0).click()
         cy.wait(500)
-        cy.get('div[class="gh-list-row gh-posts-list-item"]').click()
-        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++        
+        cy.get('section.content-list > ol > li.gh-list-row.gh-posts-list-item').click()
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++        
         cy.get('button[class="post-settings"]').click()
+        cy.wait(1000)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++        
         cy.get('button[class="gh-btn gh-btn-hover-red gh-btn-icon settings-menu-delete-button"]').click()
+        cy.wait(1000)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++        
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').click()
-        cy.wait(100)
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++
         cy.get('div.gh-nav-bottom').click()
         cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V3-' + index) 
+        index++        
         cy.get('a[href="#/signout/"]').click()
     })
 })
--- cypress/integration/Delete_page-4.40.js
+++ cypress/integration/Delete_page-4.40.js
@@ -2,6 +2,8 @@ let user = '';
 let password = '';
 let inviteUser='';
 let postTitle= '';
+let index = 1;
+let port = '3002';
 
 describe('New post', () => {
     beforeEach(()=>{        
@@ -12,31 +14,48 @@ describe('New post', () => {
             inviteUser = data.inviteUser
             postTitle = data.postTitle
         })
-        cy.visit('http://localhost:3002/ghost/')
+        cy.visit(`http://localhost:${port}/ghost/`)
         cy.wait(1000)        
         
     })
     it('Login, Test Delete Page', () => {       
         cy.get('#ember7').type(user)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++
         cy.get('#ember9').type(password)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++
         cy.get('#ember11').click() 
-        cy.wait(100)       
-        cy.url().should('eq', 'http://localhost:3002/ghost/#/dashboard')
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
+        cy.url().should('eq', `http://localhost:${port}/ghost/#/dashboard`)
         cy.get('a[href="#/pages/"]').eq(0).click()   
         cy.wait(500)
-        cy.get('div[class="gh-content-entry-title"]').click() 
-        cy.wait(100) 
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
+        cy.get('section.content-list > ol > li.gh-list-row.gh-posts-list-item').eq(0).click()
+        cy.wait(500) 
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
         cy.get('button[class="settings-menu-toggle gh-btn gh-btn-editor gh-btn-icon icon-only gh-btn-action-icon"]').click()
-        cy.wait(100)
-        cy.get('button[class="gh-btn gh-btn-hover-red gh-btn-icon settings-menu-delete-button"]').click()
-        cy.wait(100)
-        cy.get('button[class="gh-btn gh-btn-hover-red gh-btn-icon settings-menu-delete-button"]').click()
-        cy.wait(100)
+        cy.wait(1000)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
+        cy.get('button[class="gh-btn gh-btn-hover-red gh-btn-icon settings-menu-delete-button"]').click()        
+        cy.wait(1000)
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
         cy.get('button[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').click()
-        cy.wait(100)
-        cy.reload()
+        cy.wait(1000)   
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++             
         cy.get('div[class="flex-auto flex items-center"]').click()
-        cy.wait(100);
+        cy.wait(500);
+        cy.screenshot('vrt/Scenario5'  + '/Scenario5' +'_V4-' + index) 
+        index++        
         cy.get('a[href="#/signout/"]').click()  
         
     })
