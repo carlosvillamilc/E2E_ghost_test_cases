46663c871dbf163d4678ade64f4bb99a49e381e6
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-05-24 23:37:04 -0500

    Ajuste escenario 4 new page para v3 y v4

--- cypress/integration/New_page-4.40.js
+++ cypress/integration/New_page-4.40.js
@@ -2,8 +2,9 @@ let user = '';
 let password = '';
 let inviteUser='';
 let postTitle= '';
-
-describe('New page', () => {
+let port = '3002'
+let index = 1;
+describe('New page V4', () => {
     beforeEach(()=>{        
         cy.fixture('ghost.json').as('ghost')
         cy.fixture('ghost.json').then((data)  => {
@@ -12,33 +13,55 @@ describe('New page', () => {
             inviteUser = data.inviteUser
             postTitle = data.postTitle
         })
-        cy.visit('http://localhost:3002/ghost/')
+        cy.visit(`http://localhost:${port}/ghost/`)
         cy.wait(1000)        
         
     })
     it('Login, Test crear Page', () => {       
         cy.get('#ember7').type(user)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('#ember9').type(password)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('#ember11').click() 
-        cy.wait(100)       
-        cy.url().should('eq', 'http://localhost:3002/ghost/#/dashboard')
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++        
+        cy.url().should('eq', `http://localhost:${port}/ghost/#/dashboard`)
         cy.get('a[href="#/pages/"]').eq(0).click()   
-        cy.wait(500)
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('a[href="#/editor/page/"]').eq(0).click() 
-        cy.wait(100) 
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++ 
         cy.get('[placeholder="Page title"]').type(postTitle)
         cy.wait(100)
         cy.get('[data-placeholder="Begin writing your page..."]').type("Nuevo Page Texto")
-        cy.wait(100)
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('div[class="gh-publishmenu ember-view"]').click()
-        cy.wait(100)
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('button[class="gh-btn gh-btn-black gh-publishmenu-button gh-btn-icon ember-view"]').click()
-        cy.wait(100)
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('a[href="#/pages/"]').eq(0).click()
-        cy.wait(1500)
-        cy.reload()
+        cy.wait(500)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
+        cy.wait(3000)
         cy.get('div[class="flex-auto flex items-center"]').click()
-        cy.wait(100);
+        cy.wait(500);
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V4-' + index) 
+        index++
         cy.get('a[href="#/signout/"]').click()  
         
     })
--- cypress/integration/New_page.js
+++ cypress/integration/New_page.js
@@ -2,6 +2,8 @@ let user = '';
 let password = '';
 let inviteUser='';
 let postTitle= '';
+let port = '3001';
+let index = 1;
 
 describe('New page', () => {
     beforeEach(()=>{        
@@ -12,26 +14,55 @@ describe('New page', () => {
             inviteUser = data.inviteUser
             postTitle = data.postTitle
         })
-        cy.visit('http://localhost:2368/ghost/')
+        cy.visit(`http://localhost:${port}/ghost/`)
         cy.wait(2000)        
         
     })
     it('Login, Test crear Post', () => {       
         cy.get('#ember8').type(user)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
         cy.get('#ember10').type(password)
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
         cy.get('#ember12').click()        
-        cy.url().should('eq', 'http://localhost:2368/ghost/#/site')
+        cy.wait(100)
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
+        cy.url().should('eq', `http://localhost:${port}/ghost/#/site`)
         cy.get('a[href="#/pages/"]').eq(0).click();    
-        cy.wait(500)
-        cy.get('a[href="#/editor/page/"]').eq(0).click();  
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
+        cy.get('a[href="#/editor/page/"]').eq(0).click();
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
         cy.get('[placeholder="Page Title"]').type(postTitle)
         cy.wait(100)
         cy.get('[data-placeholder="Begin writing your page..."]').type("Nuevo Post Texto")
-        cy.wait(100)
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
         cy.get('div[class="ember-view ember-basic-dropdown-trigger  gh-btn gh-btn-outline gh-publishmenu-trigger"]').click()
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
         cy.get('button[class="gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view"]').click()
-        //cy.get('div[class="gh-nav-bottom"]').click();
-        //cy.wait(100);
-        //cy.get('a[href="#/signout/"]').click();   
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
+        cy.get('a[href="#/pages/"]').click();    
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++
+        cy.wait(2000)
+        cy.get('div.gh-nav-bottom').click()
+        cy.wait(500)        
+        cy.screenshot('vrt/Scenario4'  + '/Scenario4' +'_V3-' + index) 
+        index++        
+        cy.get('a[href="#/signout/"]').click();   
     })
   })
