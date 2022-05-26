ef2583ff1ec04b7d180d2c2e2106d15a3d25d44a
Carlos Villamil <c.villamilc@uniandes.edu.co>
2022-05-25 22:34:25 -0500

    Ajuste escenario scheduled post

--- cypress/integration/Scheduled_post.js
+++ cypress/integration/Scheduled_post.js
@@ -21,71 +21,64 @@ describe('Scheduled post', () => {
         cy.get('#ember8').type(user)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
 
         cy.get('#ember10').type(password)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('#ember12').click() 
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
                
         cy.url().should('eq', `http://localhost:${port}/ghost/#/site`)
-        cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
-        index++
-        cy.wait(100)
-        
         cy.get('a[href="#/posts/"]').eq(0).click();
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
             
         cy.get('a[href="#/posts/?type=scheduled"]').eq(0).click();
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
          
         cy.get('a[href="#/editor/post/"]').eq(0).click();
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
           
         cy.get('[placeholder="Post Title"]').type(postTitle)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('[data-placeholder="Begin writing your post..."]').type("Nuevo Post Texto")
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('div[class="ember-view ember-basic-dropdown-trigger  gh-btn gh-btn-outline gh-publishmenu-trigger"]').click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('button[class="gh-btn gh-btn-blue gh-publishmenu-button gh-btn-icon ember-view"]').click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
 
-        cy.get('a[href="#/posts/"]').eq(0).click();
+        cy.get('a[href="#/posts/?type=scheduled"]').eq(0).click();
+        cy.wait(500)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(2000)
         
         cy.get('div.gh-nav-bottom').click();
+        cy.wait(500)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
         index++
-        cy.wait(100)
-
-        cy.get('a[href="#/signout/"]').click();
-        cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V3-' + index) 
-        index++
-        cy.wait(100)   
+        cy.get('a[href="#/signout/"]').click();                
     })
   })
--- cypress/integration/Scheduled_post_V_4.44.0.js
+++ cypress/integration/Scheduled_post_V_4.44.0.js
@@ -20,28 +20,28 @@ describe('New post', () => {
         cy.get('#ember7').type(user)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('#ember9').type(password)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('#ember11').click() 
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
                
         cy.url().should('eq', `http://localhost:${port}/ghost/#/dashboard`)
         cy.get('a[href="#/posts/"]').eq(0).click();    
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('a[href="#/posts/?type=scheduled"]').eq(0).click(); 
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
          
         cy.get('a[href="#/editor/post/"]').eq(0).click();
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
@@ -51,43 +51,37 @@ describe('New post', () => {
         cy.get('[placeholder="Post title"]').type(postTitle)
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('[data-placeholder="Begin writing your post..."]').type("Nuevo Post Texto")
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('div[class="gh-publishmenu ember-view"]').click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('button[class="gh-btn gh-btn-black gh-publishmenu-button gh-btn-icon ember-view"]').click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
         
         cy.get('button[class="gh-btn gh-btn-black gh-btn-icon ember-view"]').click()
-        cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
-        index++
-        cy.wait(100)
+        //cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
+        cy.wait(500)
         
         cy.get('a[href="#/posts/?type=scheduled"]').eq(0).click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(2000)
         
-        cy.reload()
         cy.get('div[class="flex-auto flex items-center"]').click()
         cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
         index++
-        cy.wait(100)
+        cy.wait(500)
     
-        cy.get('a[href="#/signout/"]').click()  
-        cy.screenshot('vrt/Scenario2'  + '/Scenario2' +'_V4-' + index) 
-        index++
-        cy.wait(100)
-        
+        cy.get('a[href="#/signout/"]').click()          
     })
   })
